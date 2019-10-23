1d4868e7cfbf45d53fbd357a9374c939f04ca314
Plokasu <plokasu88@gmail.com>
2019-10-23 13:32:20 -0700

    Create SockMerchant.php
    https://www.hackerrank.com/challenges/sock-merchant/problem

---
+++ SockMerchant.php
@@ -0,0 +1,18 @@
+function sockMerchant($n, $ar) {
+    $sockTypeNumbers = [];
+    $totalPairs = 0;
+
+    for($i = 0; $i < count($ar); $i++) {
+        if(!isset($sockTypeNumbers[$ar[$i]])) {
+            $sockTypeNumbers[$ar[$i]] = 1;
+        }else{
+            $sockTypeNumbers[$ar[$i]]++;
+        }
+    }
+    
+    foreach ($sockTypeNumbers as $totalSockType) {
+        $totalPairs += (int) ($totalSockType/2);
+    }
+    
+    return $totalPairs;
+}
