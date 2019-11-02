7ea30ea93acfe0ae458216922973718ce7d0b1d6
Plokasu <plokasu88@gmail.com>
2019-11-02 15:03:03 -0700

    Create SherlockAndTheValidString.php

---
+++ SherlockAndTheValidString.php
@@ -0,0 +1,33 @@
+// https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+// Complete the isValid function below.
+function isValid($s) {
+    $keys = array_count_values(preg_split('//u', $s,-1, PREG_SPLIT_NO_EMPTY));
+    $frequencyCount = [];
+
+    foreach($keys as $letter => $frequency) {
+        if(!isset($frequencyCount[$frequency])) {
+            $frequencyCount[$frequency] = 0;
+        }
+        $frequencyCount[$frequency]++;
+    }
+
+   $length = count($frequencyCount);
+
+   if($length <= 1) {
+       return "YES";
+   }else if ($length > 2) {
+       return "NO";
+   }
+
+   $keys = array_keys($frequencyCount);
+
+   if( 
+       ($keys[0] == 1 && $frequencyCount[$keys[0]] == 1) ||
+       ($keys[1] == 1 && $frequencyCount[$keys[1]] == 1) ||
+       ($keys[1] == $keys[0] -1 &&  $frequencyCount[$keys[0]] == 1) ||
+       ($keys[0] == $keys[1] -1 &&  $frequencyCount[$keys[1]] == 1)
+     ) {
+       return "YES";
+   }
+   return "NO";
+}
